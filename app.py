31361c1a33ad4a6e81bd88ce68b888aebe0fc2ef
mehradans92 <mehrad.ans@gmail.com>
2023-11-09 23:43:17 -0500

    added visualizer

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from contextlib import redirect_stdout
 import time
 import json
 import io
+import utils
 
 
 # Initialize session state for logging if it's not already defined
@@ -38,7 +39,11 @@ with st.sidebar:
     st.markdown(
         "This tool allows you to extract information on water stability property of metal-organic frameworks from a scientific paper or input txt. This AI chemist agent uses OpenAI's GPT models, so you must have your own API key. Each query is about 16k tokens, which costs about only $0.50 on your own API key, charged by OpenAI."
     )
-    st.markdown("Used libraries:\n * [Eunomia](https://github.com/AI4ChemS/Eunomia)")
+    st.markdown(
+        "Used libraries:\n * [Eunomia](https://github.com/AI4ChemS/Eunomia)\
+               \n * [py3Dmol](https://william-dawson.github.io/using-py3dmol.html)\
+               \n * [pymatgen](https://pymatgen.org/)"
+    )
 
 
 text_input = ""
@@ -61,76 +66,98 @@ with col1:
 
 def main():
     # Radio button for user choice between uploading a file and entering text
-    input_method = st.radio("Choose input method:", ("Upload PDF file", "Input text"))
-
-    uploaded_file = None
-    text_input = None
-
-    # Conditional logic to display the file uploader or the text input area based on the radio button selection
-    if input_method == "Upload PDF file":
-        uploaded_file = st.file_uploader(
-            "Drag and drop or click to upload a file (PDF only)", type="pdf"
-        )
-    elif input_method == "Input text":
-        text_input = st.text_area("Enter your text here:")
-
-    # A button to trigger the processing
-    process_button = st.button("Extract Information")
-
-    if process_button:
-        if input_method == "Upload PDF file" and uploaded_file is not None:
-            # Process the uploaded PDF
-            with NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-                tmp_file.write(uploaded_file.getvalue())
-                file_path = tmp_file.name
-            docs_processor = eunomia.LoadDoc(file_name=file_path, encoding="utf8")
-            sliced_pages = docs_processor.process(
-                ["references ", "acknowledgement", "acknowledgments", "references\n"],
-                chunk_size=1000,
-                chunk_overlap=50,
-                chunking_type="fixed-size",
-            )
-            st.write("PDF processed.")
-        elif input_method == "Input text" and text_input:
-            # Process the input text
-            docs_processor = eunomia.LoadDoc(text_input=text_input)
-            sliced_pages = docs_processor.process(
-                chunk_size=600, chunk_overlap=50, chunking_type="NLTK"
-            )
-            st.write("Text processed.")
-
-        Embedding_model = "text-embedding-ada-002"
-        faiss_index = FAISS.from_documents(
-            sliced_pages, OpenAIEmbeddings(model=Embedding_model)
+    with st.form(key="columns_in_form", clear_on_submit=False):
+        input_method = st.radio(
+            "Choose input method:", ("Upload PDF file", "Input text")
         )
 
-        tools = EunomiaTools(
-            tool_names=["read_doc", "eval_justification", "recheck_justification"],
-            vectorstore=faiss_index,
-        ).get_tools()
-        agent = Eunomia(tools=tools, model="gpt-4", get_cost=True)
+        uploaded_file = None
+        text_input = None
 
-        stdout_buffer = io.StringIO()
-        with redirect_stdout(stdout_buffer):
-            with st.spinner("⏳ Please wait..."):
-                start = time.time()
-                result = agent.run(
-                    prompt="Read the document and find the MOFS and their water stability.\
-                            \Check your answers and if the reasoning is not valid, try to find better reasons.\
-                            Your final answer should be in a parsable JSON format."
+        # Conditional logic to display the file uploader or the text input area based on the radio button selection
+        if input_method == "Upload PDF file":
+            uploaded_file = st.file_uploader(
+                "Drag and drop or click to upload a file (PDF only)", type="pdf"
+            )
+        elif input_method == "Input text":
+            text_input = st.text_area("Enter your text here:")
+
+        # A button to trigger the processing
+        process_button = st.form_submit_button("Extract Information")
+
+        if process_button:
+            if input_method == "Upload PDF file" and uploaded_file is not None:
+                # Process the uploaded PDF
+                with NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+                    tmp_file.write(uploaded_file.getvalue())
+                    file_path = tmp_file.name
+                docs_processor = eunomia.LoadDoc(file_name=file_path, encoding="utf8")
+                sliced_pages = docs_processor.process(
+                    [
+                        "references ",
+                        "acknowledgement",
+                        "acknowledgments",
+                        "references\n",
+                    ],
+                    chunk_size=1000,
+                    chunk_overlap=50,
+                    chunking_type="fixed-size",
                 )
-                # Pretty print the JSON with an indent of 4
-                final_answer = json.loads(result)
-                pretty_json = json.dumps(final_answer, indent=4)
-                length_answer = len(pretty_json)
-                st.success("Voila! 😃")
-                st.text_area(
-                    "Materials found:", pretty_json, height=max(length_answer // 4, 300)
+                st.write("PDF processed.")
+            elif input_method == "Input text" and text_input:
+                # Process the input text
+                docs_processor = eunomia.LoadDoc(text_input=text_input)
+                sliced_pages = docs_processor.process(
+                    chunk_size=600, chunk_overlap=50, chunking_type="NLTK"
                 )
-                end = time.time()
-                clock_time = end - start
-                with st.empty():
-                    st.write(f"✔️ Task completed in {clock_time:.2f} seconds.")
+                st.write("Text processed.")
+
+            Embedding_model = "text-embedding-ada-002"
+            faiss_index = FAISS.from_documents(
+                sliced_pages, OpenAIEmbeddings(model=Embedding_model)
+            )
+
+            tools = EunomiaTools(
+                tool_names=["read_doc", "eval_justification", "recheck_justification"],
+                vectorstore=faiss_index,
+            ).get_tools()
+            agent = Eunomia(tools=tools, model="gpt-4", get_cost=True)
+
+            stdout_buffer = io.StringIO()
+            with redirect_stdout(stdout_buffer):
+                with st.spinner("⏳ Please wait..."):
+                    start = time.time()
+                    result = agent.run(
+                        prompt="Read the document and find the MOFS and their water stability.\
+                                \Check your answers and if the reasoning is not valid, try to find better reasons.\
+                                Your final answer should be in a parsable JSON format."
+                    )
+                    # Pretty print the JSON with an indent of 4
+                    final_answer = json.loads(result)
+                    pretty_json = json.dumps(final_answer, indent=4)
+                    length_answer = len(pretty_json)
+                    st.success("Voila! 😃")
+                    st.text_area(
+                        "Materials found:",
+                        pretty_json,
+                        height=max(length_answer // 4, 300),
+                    )
+                    end = time.time()
+                    clock_time = end - start
+                    with st.empty():
+                        st.write(f"✔️ Task completed in {clock_time:.2f} seconds.")
+
+    with st.form(key="visualize_mof", clear_on_submit=False):
+        cif_file = st.file_uploader(
+            "Drag and drop or click to upload a file (CIF only)", type="CIF"
+        )
+        # A button to trigger the processing
+        vis_button = st.form_submit_button("Visualize MOF")
+        if vis_button:
+            with NamedTemporaryFile(delete=False, suffix=".cif") as tmp_file:
+                tmp_file.write(cif_file.getvalue())
+            cif_file_path = tmp_file.name
+            utils.visualize(cif_file_path)
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,22 @@
+def visualize(cif_path):
+    from pymatgen.io.cif import CifParser
+    from pymatgen.io.xyz import XYZ
+    import py3Dmol
+
+    # Replace 'path_to_your_file.cif' with the path to your CIF file
+    cif_path = "qmof-000dce3.cif"
+    parser = CifParser(cif_path)
+    structure = parser.get_structures()[0]
+
+    # Convert the structure to an XYZ format string
+    xyz = XYZ(structure).__str__()
+
+    # Set up the py3Dmol view
+    view = py3Dmol.view(width=800, height=400)
+    view.addModel(xyz, "xyz")
+    view.setStyle({"stick": {"radius": 0.15}, "sphere": {"radius": 0.5}})
+
+    from stmol import showmol
+
+    view.zoomTo()
+    showmol(view, height=500, width=800)
