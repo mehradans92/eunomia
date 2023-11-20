a591110d2f30d9abdbbbef5a38448279e5e8f0b1
mehradans92 <mehrad.ans@gmail.com>
2023-11-20 18:34:32 -0500

    removed comments

--- app.py
+++ app.py
@@ -5,12 +5,9 @@ import eunomia
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from eunomia.agents import Eunomia
-from eunomia.tools import EunomiaTools
 from tempfile import NamedTemporaryFile
-from contextlib import redirect_stdout
 import time
 import json
-import io
 import utils
 
 
@@ -54,52 +51,6 @@ col1, col2, col3 = st.columns([1, 2, 3])
 with col1:
     st.image(image, width=800)
 
-# input_prompt = """You are an expert chemist. This document describes the water stability property of few MOFs.
-#             For each MOF mentioned find the following:
-#             1. The water stability of the MOF.
-#             2. The exact sentences without any changes from the document that justifies your decision.
-#               Try to find more than once sentence.
-#             This should be "Not provided" if you cannot find the water stability for any MOF you find."""
-
-input_prompt = """You are an expert chemist. This document describes the VEGFR affinity property of few peptide sequences.
-            For each peptide sequence mentioned, find the following:
-            1. The VEGFR affinity of the peptide sequence.
-            2. The exact sentences without any changes from the document that justifies your decision.
-              Try to find more than once sentence.
-            This should be "Not provided" if you cannot find the  VEGFR affinity for any peptide sequence you find.
-"""
-
-# chem_rules = f"""
-# There are only 3 options for water stability:
-# 1. Stable: No change in properties after exposure to moisture or steam, soaking or boiling in water or
-#  an aqueous solution.
-# Retaining its porous structure in solution.
-# No loss of crystallinity.
-# insoluble in water or an aqueous solution.
-# Water adsorption isotherm should have a steep uptake.
-# Good cycling performance.
-
-# 2. Unstable
-# The MOF will decompose or change properties or has a change in its crystal structure after exposure/soak to a
-#  humid environment, steam or if it partially dissolves in water.
-# Soluble or patrially soluble in water or an aqueous solution.
-
-
-# 3. Not provided,
-# If you don't know or cannot justify 1 or 2
-# """
-chem_rules = f"""A valid peptide sequence should only contain the 20 essential amino acids:
-"A, R, N, D, C, Q, E, G, H, I, L, K, M, F, P, S, T, W, Y, V"
-"""
-
-example_output_formatting_rules = """Your final answer should look like:            {
-peptide sequence:  VEGFR affinity
- justification: exact sentence from the context.
-}"""
-# example_output_formatting_rules = "Your final answer should look like:\
-#             MOF name: water stability (Stable, Unstable, Not provided)\
-#             justification: exact sentence from the context."
-
 
 def main():
     col1, col2 = st.columns([1, 1])
@@ -138,11 +89,6 @@ def main():
         )
     # Radio button for user choice between uploading a file and entering text
     input_method = st.radio("Choose input method:", ("Upload file", "Input text"))
-    # # Add a checkbox for the chain-of-verification
-    # chain_of_verification = st.checkbox(
-    #     "Add Chain-of-Verification (CoV)",
-    #     help=f"Adds an iterative process of verifying agent's answers. Note that this will increase the running time of your information extraction, but will result better answers.",
-    # )
 
     with st.form(
         key="columns_in_form",
@@ -191,14 +137,7 @@ def main():
             faiss_index = FAISS.from_documents(
                 sliced_pages, OpenAIEmbeddings(model=Embedding_model)
             )
-            # Dynamic tool names list based on checkbox
-            # tool_names = ["read_doc"]
-            # if chain_of_verification:
-            #     tool_names.extend(["eval_justification", "recheck_justification"])
-            # tools = EunomiaTools(
-            #     tool_names=tool_names,
-            #     vectorstore=faiss_index,
-            # ).get_tools()
+
             tool_kits = utils.tools_generator(
                 material, property, prompt, faiss_index, rules
             )
@@ -228,18 +167,6 @@ def main():
                 clock_time = end - start
                 with st.empty():
                     st.write(f"✔️ Task completed in {clock_time:.2f} seconds.")
-
-    # with st.form(key="visualize_mof", clear_on_submit=False):
-    #     cif_file = st.file_uploader(
-    #         "Drag and drop or click to upload a file (CIF only)", type="CIF"
-    #     )
-    #     # A button to trigger the processing
-    #     vis_button = st.form_submit_button("Visualize MOF")
-    #     if vis_button:
-    #         with NamedTemporaryFile(delete=False, suffix=".cif") as tmp_file:
-    #             tmp_file.write(cif_file.getvalue())
-    #         cif_file_path = tmp_file.name
-    #         utils.visualize(cif_file_path)
 
 
 if __name__ == "__main__":
